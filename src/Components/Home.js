1b131880e9c89f61d8714aac3e3cfd40c85186ed
knyg <kevin1992yang@gmail.com>
2018-12-08 20:52:00 -0800

    testing animations

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
     state = {
-        homeClass: 'slide-in-top',
         homeStyle: {
             textAlign: 'center',
-            paddingTop: '15%'
+            paddingTop: '15%',
+            fontSize: '2em'
         }
     };
 
     render() {
         return (
-            <div className={this.state.homeClass} style={this.state.homeStyle}>
-                Welcome to my page!
+            <div className='homeWrapper' style={this.state.homeStyle}>
+                <span className='slide-in-top'>H</span>
+                <span className='slide-in-top2'>E</span>
+                <span className='slide-in-top3'>L</span>
+                <span className='slide-in-top4'>L</span>
+                <span className='slide-in-top5'>O</span>
             </div>
         );
     };
