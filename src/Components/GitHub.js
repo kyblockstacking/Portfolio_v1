927ecd579eee1dcfdc484621238627c78b8f3ac2
knyg <kevin1992yang@gmail.com>
2019-01-04 03:10:42 -0800

    style github paper

--- src/Components/GitHub.js
+++ src/Components/GitHub.js
@@ -6,13 +6,43 @@ export default class GitHub extends Component {
         GitHubStyle: {
             marginLeft: 'auto',
             marginRight: 'auto',
+        },
+        body: {
+            backgroundColor: '#f5f5f5',
+            width: '600px',
+            margin: '0 auto',
+            padding: '0',
+        },
+        list: {
+            color: '#555',
+            fontSize: '22px',
+            padding: '0 !important',
+            width: '500px',
+            fontFamily: 'courier, monospace',
+            border: '1px solid #dedede',
         }
+
     };
 
     render() {
         return (
-            <div style={this.state.GitHubStyle}>
-                GITHUB
+            <div className='paper'>
+                <div>GITHUB</div>
+                <div class="lines" />
+                <ul class="list">
+                    <li>Projects</li>
+                    <li>ThreaderZZZ</li>
+                    <li>Pickee</li>
+                    <li>Eat lunch</li>
+                    <li>Call mom</li>
+                    <li>Tweet about feeding my cow, pugsly</li>
+                    <li>Join a hangout in google+</li>
+                    <li>Prepare Dinner</li>
+                    <li>Eat Dinner</li>
+                    <li>Get ready for bed</li>
+                    <li>Go to bed</li>
+                </ul>
+
             </div>
         );
     };
