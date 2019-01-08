38fd982fe62aa7bb69a0db2db8085b90a27ca3ef
knyg <kevin1992yang@gmail.com>
2019-01-08 06:06:05 -0800

    add delay project animation

--- src/Components/Stack.js
+++ src/Components/Stack.js
@@ -2,13 +2,7 @@ import React, { Component } from 'react';
 import Carousel from './Carousel';
 
 class Stack extends Component {
-    constructor(props) {
-        super();
-
-        this.state = {};
-
-    };
-
+    
     render() {
         return (
             <div>
