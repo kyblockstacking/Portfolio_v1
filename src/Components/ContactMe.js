18132360ac7fc9472831755322a3e642982e98de
Kevin Yang <40349734+kyblockstacking@users.noreply.github.com>
2019-02-01 18:46:18 -0800

    Update ContactMe.js

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -17,7 +17,7 @@ class ContactMe extends Component {
             <div style={this.state.indexCard} className='IndexCardWrapper index-card slide-in-left'>
                 <ul className='index-list'>
                     <li style={{ border: 'none', padding: '0.75em', textAlign: 'center' }}>Contact Information</li>
-                    <li style={{ borderTop: '3px double #c93030', textAlign: 'center' }}>Full Stack Web Developer</li>
+                    <li style={{ borderTop: '3px double #c93030', textAlign: 'center' }}>Full-Stack Web Developer</li>
                     <li>&nbsp;</li>
                     <li>Name: Kevin Yang</li>
                     <li>Email: kevin1992yang@gmail.com</li>
